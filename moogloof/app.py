c178b8099d80ad76bcce33c86f04afcf6a98e91a
Ethan Yun <ethanyun@ohs.stanford.edu>
2020-12-08 08:51:37 -0800

    More comments

--- moogloof/app.py
+++ moogloof/app.py
@@ -1,10 +1,14 @@
+# Package imports
 from flask import Flask
 
+# App imports
 from moogloof.config import SECRET_KEY
 
+# Create app
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
+# App functions
 import moogloof.context_processors
 import moogloof.views
 
