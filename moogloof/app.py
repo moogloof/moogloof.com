4566156214feb11a2182214170c560dfc36404fb
Ethan Yun <ethanyun@ohs.stanford.edu>
2021-05-14 15:15:51 -0700

    Unecessary config stuff

--- moogloof/app.py
+++ moogloof/app.py
@@ -2,12 +2,10 @@
 from flask import Flask
 
 # App imports
-from moogloof.config import SECRET_KEY, SERVER_NAME
 
 # Create app
 app = Flask(__name__)
-app.secret_key = SECRET_KEY
-app.config["SERVER_NAME"] = SERVER_NAME
+app.config.from_pyfile("config.py")
 
 # App functions
 import moogloof.context_processors
