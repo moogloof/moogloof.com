d6a84ecb9c4d91ba190fbfffbbcf28ae126f1f4b
Ethan Yun <ethanyun@ohs.stanford.edu>
2022-03-17 11:44:14 -0700

    Markdown for the blog posts

--- moogloof/app.py
+++ moogloof/app.py
@@ -1,11 +1,14 @@
 # Package imports
 from flask import Flask
+from flaskext.markdown import Markdown
 
 # App imports
 
 # Create app
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
+
+md = Markdown(app, extensions=["fenced_code"])
 
 # App functions
 import moogloof.context_processors
